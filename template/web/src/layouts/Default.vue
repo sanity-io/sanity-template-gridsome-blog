f7c1563e48fc463bd316496643543fd15c6103c5
Knut Melvær <knut.melvaer@gmail.com>
2019-09-12 00:32:40 +0200

    add missing quotation mark

--- template/web/src/layouts/Default.vue
+++ template/web/src/layouts/Default.vue
@@ -17,7 +17,7 @@
 
     <footer class="footer">
       <span class="footer__copyright">Copyright © {{ new Date().getFullYear() }}. </span>
-      <span class="footer__links">Powered by <a href="//gridsome.org"> Gridsome </a> & <a href="//www.sanity.io> Sanity.io </a></span>
+      <span class="footer__links">Powered by <a href="//gridsome.org"> Gridsome </a> & <a href="//www.sanity.io"> Sanity.io </a></span>
     </footer>
 
   </div>
