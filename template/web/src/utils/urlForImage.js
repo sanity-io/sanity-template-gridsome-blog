e0f149387a02ee0b2cc06a23b696151842c61c2c
Espen Hovlandsdal <espen@hovlandsdal.com>
2019-09-11 18:19:12 -0700

    Use DPR-aware image URLs

--- template/web/src/utils/urlForImage.js
+++ template/web/src/utils/urlForImage.js
@@ -1,8 +1,11 @@
 import urlBuilder from '@sanity/image-url'
 
 const urlForImage = (source, data) => {
-  const {projectId, dataset} = data
-  return urlBuilder({projectId, dataset}).image(source)
+  const isBrowser = typeof window !== 'undefined'
+  const { projectId, dataset } = data
+  const builder = urlBuilder({ projectId, dataset }).image(source)
+  const dpr = isBrowser ? Math.min(Math.ceil(window.devicePixelRatio || 1), 3) : 1
+  return dpr > 1 ? builder.dpr(dpr) : builder
 }
 
 export default urlForImage
